fc5a7d57edf8d5133a8d996e03d72631b33970d4
Alaz <arianzakizadeh@gmail.com>
2022-09-27 01:59:46 +0200

    Add files via upload

---
+++ freak/freak.c
@@ -0,0 +1,51 @@
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+//////                                                                //////
+//////   ||||||||||  ||||||||| |||||||||      ||||       ||     ||    //////
+//////   |||         ||     || ||            ||  ||      ||   ||      //////
+//////   ||||||||||  ||||||||| ||           ||    ||     || ||        //////
+//////   |||         ||||      |||||||||   ||||||||||    || ||        //////
+//////   |||         ||  ||    ||         ||        ||   ||   ||      //////
+//////   |||         ||    ||  ||||||||| ||          ||  ||     ||    //////
+//////                  version 0.0.0    *ALAZ*                       //////
+////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////
+
+#include <unistd.h>
+#include <stdlib.h>
+#include "minilibx_macos/mlx.h"
+
+typedef struct freak
+{
+	void	*mlx;
+	void	*mlx_win;
+}			t_freak;
+
+int	key(int key)
+{
+	if (key >= 0 && key <= 150000)
+		system("pmset displaysleepnow");
+		exit(1);
+	return 0;
+}
+
+int	ft_close(void *data)
+{
+	(void)data;
+		system("pmset displaysleepnow");
+		exit(1);
+	return 0;
+}
+
+int main(void) 
+{
+	t_freak	*data;
+
+	data = (t_freak *)malloc(sizeof(t_freak));
+	data->mlx = mlx_init();
+	data->mlx_win = mlx_new_window(data->mlx, 2560, 1900, "Freak!");
+	mlx_key_hook(data->mlx_win, key, data);
+	mlx_hook(data->mlx_win, 6, 0, ft_close, data);
+	mlx_loop(data->mlx);
+	return 0;
+}
